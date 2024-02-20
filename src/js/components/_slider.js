a5cf547a1e353c8830fbb41d0565d3675b5c31fa
TaVitka <tavitka17@gmail.com>
2024-02-20 20:05:37 +0200

    created explore section for about page + removed unneeded files

--- src/js/components/_slider.js
+++ src/js/components/_slider.js
@@ -1,31 +1,6 @@
 import Swiper, { Navigation, Pagination, EffectFade, Scrollbar } from 'swiper';
 
-const swiperTeam = new Swiper('.team-slider', {
-  slidesPerView: 1,
-  spaceBetween: 20,
-  modules: [Navigation, Pagination],
-  pagination: {
-    clickable: true,
-    el: '.swiper-pagination',
-  },
-
-  draggable: true,
-  grabCursor: true,
-  navigation: {
-    nextEl: '.team .btn--next',
-    prevEl: '.team .btn--prev',
-  },
-  breakpoints: {
-    992: {
-      slidesPerView: 3,
-    },
-    650: {
-      slidesPerView: 2,
-    },
-  },
-});
-
-const swiperReviews = new Swiper('.reviews__slider', {
+const swiperTeam = new Swiper('.explore__slider', {
   slidesPerView: 1,
   speed: 500,
   effect: 'fade',
@@ -33,14 +8,67 @@ const swiperReviews = new Swiper('.reviews__slider', {
     delay: 200,
   },
   modules: [Navigation, Scrollbar, Pagination, EffectFade],
-  scrollbar: {
-    el: '.swiper-scrollbar',
-    draggable: true,
-  },
   draggable: true,
   grabCursor: true,
   navigation: {
-    nextEl: '.reviews .btn--next',
-    prevEl: '.reviews .btn--prev',
+    nextEl: '.explore__slider .btn--next',
+    prevEl: '.explore__slider .btn--prev',
   },
+  pagination: {
+    // clickable: true,
+    el: '.explore__slider .swiper-pagination',
+  },
+  // breakpoints: {
+  //   992: {
+  //     slidesPerView: 3,
+  //   },
+  //   650: {
+  //     slidesPerView: 2,
+  //   },
+  // },
 });
+
+// const swiperTeam = new Swiper('.team-slider', {
+//   slidesPerView: 1,
+//   spaceBetween: 20,
+//   modules: [Navigation, Pagination],
+//   pagination: {
+//     clickable: true,
+//     el: '.swiper-pagination',
+//   },
+//
+//   draggable: true,
+//   grabCursor: true,
+//   navigation: {
+//     nextEl: '.team .btn--next',
+//     prevEl: '.team .btn--prev',
+//   },
+//   breakpoints: {
+//     992: {
+//       slidesPerView: 3,
+//     },
+//     650: {
+//       slidesPerView: 2,
+//     },
+//   },
+// });
+
+// const swiperReviews = new Swiper('.reviews__slider', {
+//   slidesPerView: 1,
+//   speed: 500,
+//   effect: 'fade',
+//   autoplay: {
+//     delay: 200,
+//   },
+//   modules: [Navigation, Scrollbar, Pagination, EffectFade],
+//   scrollbar: {
+//     el: '.swiper-scrollbar',
+//     draggable: true,
+//   },
+//   draggable: true,
+//   grabCursor: true,
+//   navigation: {
+//     nextEl: '.reviews .btn--next',
+//     prevEl: '.reviews .btn--prev',
+//   },
+// });
