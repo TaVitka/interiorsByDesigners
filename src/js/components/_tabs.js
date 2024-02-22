732e2d0598ee813dd8cc6242c02a5481d5e1e1aa
Orest Polusyn <o.polusyn@skarbdev.com>
2024-02-22 22:47:40 +0200

    add tabs

---
+++ src/js/components/_tabs.js
@@ -0,0 +1,33 @@
+const tabsContainer = document.querySelector('.tabs');
+const tabsList = tabsContainer.querySelector('.tabs__nav');
+const tabButtons = tabsList.querySelectorAll('a');
+const tabPanels = tabsContainer.querySelectorAll('.tabs__pane > div');
+
+tabButtons.forEach((tab, index) => {
+  tab.setAttribute('role', 'tab');
+  if (index === 0) {
+    tab.classList.add('selected');
+    // we'll add something here
+  } else {
+    tabPanels[index].setAttribute('hidden', '');
+  }
+});
+
+tabsContainer.addEventListener('click', e => {
+  const clickedTab = e.target.closest('a');
+  if (!clickedTab) return;
+  e.preventDefault();
+
+  switchTab(clickedTab);
+});
+
+function switchTab(newTab) {
+  const activePanelId = newTab.getAttribute('href');
+  const activePanel = tabsContainer.querySelector(activePanelId);
+
+  tabPanels.forEach(panel => {
+    panel.setAttribute('hidden', true);
+  });
+
+  activePanel.removeAttribute('hidden', false);
+}
